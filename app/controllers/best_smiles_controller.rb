72f1d5d3a956a101ff697d525cb515d3b4824099
tohaya0216@gmail.com <tohaya0216@gmail.com>
2017-06-11 09:10:55 +0100

    add headers

--- app/controllers/best_smiles_controller.rb
+++ app/controllers/best_smiles_controller.rb
@@ -1,4 +1,14 @@
 class BestSmilesController < ApplicationController
+
+ after_filter :cors_set_access_control_headers
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
   def create
 
     image_url = params[:url]
